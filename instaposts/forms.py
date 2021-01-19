2f9fdede9db5b24337c54d0a77cc768d4f087519
farzana583 <farzanaisack@gmail.com>
2021-01-20 00:44:55 +0300

    modified changes in   instaposts/forms.py

--- instaposts/forms.py
+++ instaposts/forms.py
@@ -7,3 +7,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user','likes']
